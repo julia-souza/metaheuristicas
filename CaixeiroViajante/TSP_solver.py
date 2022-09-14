64f4038e922d7596ba64e2c519dcbaa8d93d7198
Júlia Souza <juliadsouzaborges@gmail.com>
2022-09-14 20:23:34 -0300

    Inclusão de GRASP

--- CaixeiroViajante/TSP_solver.py
+++ CaixeiroViajante/TSP_solver.py
@@ -104,6 +104,8 @@ class TSPSolver:
         self.button_gerar_solucao = tkinter.Button(self.frame, text="gerar solução", command=self.construir_solucao_e_mostrar)
         self.button_busca_local = tkinter.Button(self.frame, text="busca local", command=self.soluciona_com_busca_local)
         self.button_ils = tkinter.Button(self.frame, text="ILS", command=self.soluciona_com_ils)
+        self.button_multistart = tkinter.Button(self.frame, text="Multistart", command=self.soluciona_com_multistart)
+        self.button_grasp = tkinter.Button(self.frame, text="GRASP", command=self.soluciona_com_grasp)
         self.canvas = tkinter.Canvas(self.frame, width=800, height=700)
         self.img = Image.new("RGB", size=(800, 700), color=(255, 255, 255))
         self.tkimg = ImageTk.PhotoImage(self.img)
@@ -119,6 +121,8 @@ class TSPSolver:
         self.button_gerar_solucao.grid( row=2, column=0,                            sticky=tkinter.W)
         self.button_busca_local.grid(   row=2, column=1,                            sticky=tkinter.W)
         self.button_ils.grid(           row=2, column=2,                            sticky=tkinter.W)
+        self.button_multistart.grid(    row=2, column=3,                            sticky=tkinter.W)
+        self.button_grasp.grid(         row=2, column=4,                            sticky=tkinter.W)
         self.canvas.grid(               row=3, column=0, columnspan=8, rowspan=5)
 
         self.root.mainloop()
@@ -295,7 +299,7 @@ class TSPSolver:
         self.passo_busca.append(len(self.passo_busca))
         self.valor_atual.append(self.custo_solucao(solucao_atual))
 
-        print("---> Busca local iniciada ---")
+        # print("---> Busca local iniciada ---")
         melhor_vizinho = [0] * self.dimension
         solucao_otima_local = [0] * self.dimension
         self.copia_solucao(solucao_atual, solucao_otima_local)
@@ -306,16 +310,16 @@ class TSPSolver:
             if encontrou_melhoria:
                 self.copia_solucao(melhor_vizinho, solucao_otima_local)
                 custo_melhor_vizinho = self.custo_solucao(melhor_vizinho)
-                print("Melhoria: Custo = {:.3f}, Solução: ".format(custo_melhor_vizinho), end='')
-                self.imprimir_solucao(melhor_vizinho)
+                # print("Melhoria: Custo = {:.3f}, Solução: ".format(custo_melhor_vizinho), end='')
+                # self.imprimir_solucao(melhor_vizinho)
 
                 self.passo_busca.append(len(self.passo_busca))
                 self.valor_atual.append(custo_melhor_vizinho)
                 
                 self.mostrar_solucao(melhor_vizinho)
                 self.canvas.update_idletasks()          # atualiza o canvas
-                time.sleep(0.5)                         # pausa para o usuário observar a mudança
-        print("---> Busca local finalizada ---\n")
+                time.sleep(0.03)                         # pausa para o usuário observar a mudança
+        # print("---> Busca local finalizada ---\n")
 
         return solucao_otima_local
 
@@ -324,8 +328,121 @@ class TSPSolver:
         solucao_final = self.busca_local(solucao_inicial)
         self.mostrar_solucao(solucao_final)
 
+    def ordena_lista(self, atual, lista):
+        for i in range(len(lista)):
+            ind_menor = i
+            valor_menor = self.matriz_adj[atual][lista[i]]
+            for j in range(i + 1, len(lista)):
+                if (self.matriz_adj[atual][lista[j]] < valor_menor):
+                    ind_menor = j
+                    valor_menor = self.matriz_adj[atual][lista[j]]
+            if ind_menor != i:
+                aux = lista[i]
+                lista[i] = lista[ind_menor]
+                lista[ind_menor] = aux
+
+    def constroi_solucao_gulosa_aleatoria(self, solucao, alfa: float):
+        '''Constroi solução gulosa com percentual de 
+           aleatoriedade alfa. Cada passo do método construtivo
+           guloso (vizinho mais próximo) deve criar uma
+           lista das cidades ainda não visitadas, ordenadas
+           pela distância para a cidade atual. Desta lista,
+           devem ser consideradas as cidades entre as alfa*100 %
+           primeiras e escolhida uma delas aleatoriamente.'''
+        visitadas = [False] * self.dimension
+        atual = random.randrange(0, self.dimension)
+        solucao[0] = atual
+        visitadas[atual] = True
+        for i in range(self.dimension - 1):
+            prox = -1
+            vizinhas = []
+            for j in range(self.dimension):
+                if j == atual or visitadas[j]:
+                    continue
+                vizinhas.append(j)
+            self.ordena_lista(atual, vizinhas)
+            max = int(len(vizinhas) * alfa)
+            if max == 0:
+                max = 1
+            ind_prox = random.randrange(0, max)
+            prox = vizinhas[ind_prox]
+
+            solucao[i + 1] = prox
+            visitadas[prox] = True
+            atual = prox
+
+    def constroi_solucao_aleatoria(self, solucao):
+        '''Preencher a lista solucao com valores de range(0, self.dimension),
+           em ordem aleatória.'''
+        for i in range(self.dimension):
+            solucao[i] = i
+        random.shuffle(solucao)
+
     def soluciona_com_multistart(self):
-        pass
+        '''Executa buscas locais a partir de soluções iniciais diferentes.'''
+        self.passo_busca = []
+        self.valor_atual = []
+
+        solucao_inicial = [0] * self.dimension
+        melhor_solucao_conhecida = [0] * self.dimension
+
+        custo_melhor_solucao_conhecida = 1e300
+        num_iteracoes = 100
+        for i in range(num_iteracoes):
+            # 1. gerar solução inicial aleatória
+            self.constroi_solucao_aleatoria(solucao_inicial)
+            # 2. realizar uma busca local
+            solucao_otima_local = self.busca_local(solucao_inicial)
+            custo_otima_local = self.custo_solucao(solucao_otima_local)
+            if custo_otima_local < custo_melhor_solucao_conhecida:
+                self.copia_solucao(solucao_otima_local, melhor_solucao_conhecida)
+                custo_melhor_solucao_conhecida = custo_otima_local
+                print("\nMelhor..: Custo = {:.3f}, Solução: ".format(custo_melhor_solucao_conhecida), end='')
+                self.imprimir_solucao(melhor_solucao_conhecida)
+
+        self.mostrar_solucao(melhor_solucao_conhecida)
+        print("\nFinal...: Custo = {:.3f}, Solução: ".format(self.custo_solucao(melhor_solucao_conhecida)), end='')
+        self.imprimir_solucao(melhor_solucao_conhecida)
+
+        plt.plot(self.passo_busca, self.valor_atual)
+        plt.xlabel("passo busca")
+        plt.ylabel("custo")
+        plt.show()
+
+        return melhor_solucao_conhecida
+
+    def soluciona_com_grasp(self):
+        '''Executa buscas locais a partir de soluções iniciais criadas com método guloso randomizado.'''
+        self.passo_busca = []
+        self.valor_atual = []
+
+        solucao_inicial = [0] * self.dimension
+        melhor_solucao_conhecida = [0] * self.dimension
+
+        custo_melhor_solucao_conhecida = 1e300
+        num_iteracoes = 100
+        for i in range(num_iteracoes):
+            # 1. gerar solução inicial aleatória
+            self.constroi_solucao_gulosa_aleatoria(solucao_inicial, 0.2)
+            # 2. realizar uma busca local
+            solucao_otima_local = self.busca_local(solucao_inicial)
+            custo_otima_local = self.custo_solucao(solucao_otima_local)
+            if custo_otima_local < custo_melhor_solucao_conhecida:
+                self.copia_solucao(solucao_otima_local, melhor_solucao_conhecida)
+                custo_melhor_solucao_conhecida = custo_otima_local
+                print("\nMelhor..: Custo = {:.3f}, Solução: ".format(custo_melhor_solucao_conhecida), end='')
+                self.imprimir_solucao(melhor_solucao_conhecida)
+
+        self.mostrar_solucao(melhor_solucao_conhecida)
+        print("\nFinal...: Custo = {:.3f}, Solução: ".format(self.custo_solucao(melhor_solucao_conhecida)), end='')
+        self.imprimir_solucao(melhor_solucao_conhecida)
+
+        plt.plot(self.passo_busca, self.valor_atual)
+        plt.xlabel("passo busca")
+        plt.ylabel("custo")
+        plt.show()
+
+        return melhor_solucao_conhecida
 
     def perturba_solucao_2_trocas_2_cidades(self, solucao_original, solucao_perturbada):
         self.copia_solucao(solucao_original, solucao_perturbada)
@@ -346,6 +463,15 @@ class TSPSolver:
         p1 = random.randrange(0, self.dimension)
         dist = random.randrange(1, self.dimension)
         p2 = (p1 + dist) % self.dimension
+
+# (Exemplo)
+# solucao: [3, 5, 2, 7, 8, 4, 6, 1]
+# indices:  0  1  2  3  4  5  6  7
+#                          ^
+#                          p1: 5 (escolhido aleatoriamente)
+# dist: 4 (escolhido aleatoriamente)
+# p2: (5 + 4) % 8 = 1
+
         self.troca_2_arestas(solucao_original, solucao_intermediaria, p1, p2)
         p1 = random.randrange(0, self.dimension)
         dist = random.randrange(1, self.dimension)
@@ -365,7 +491,7 @@ class TSPSolver:
         self.copia_solucao(solucao_otima_local, melhor_solucao_conhecida)
         iteracoes_sem_melhoria = 0
         # 3. se critério de parada não satisfeito
-        while iteracoes_sem_melhoria <= 50:
+        while iteracoes_sem_melhoria <= 100:
         # 4.    perturbar melhor solução conhecida
             #self.perturba_solucao_2_trocas_2_cidades(melhor_solucao_conhecida, solucao_inicial)
             self.perturba_solucao_2_trocas_2_arestas(melhor_solucao_conhecida, solucao_inicial)
@@ -375,11 +501,13 @@ class TSPSolver:
             custo_melhor_conhecida = self.custo_solucao(melhor_solucao_conhecida)
             if custo_otima_local < custo_melhor_conhecida:
                 self.copia_solucao(solucao_otima_local, melhor_solucao_conhecida)
+                print("\nMelhor..: Custo = {:.3f}, Solução: ".format(custo_melhor_conhecida), end='')
+                self.imprimir_solucao(melhor_solucao_conhecida)
                 iteracoes_sem_melhoria = 0
             else:
                 iteracoes_sem_melhoria += 1
         self.mostrar_solucao(melhor_solucao_conhecida)
-        print("\nMelhor..: Custo = {:.3f}, Solução: ".format(self.custo_solucao(melhor_solucao_conhecida)), end='')
+        print("\nFinal...: Custo = {:.3f}, Solução: ".format(self.custo_solucao(melhor_solucao_conhecida)), end='')
         self.imprimir_solucao(melhor_solucao_conhecida)
 
         plt.plot(self.passo_busca, self.valor_atual)
---
+++ LocalizacaoFacilidades/README.md
@@ -0,0 +1,5 @@
+[![forthebadge](https://forthebadge.com/images/badges/made-with-python.svg)](https://forthebadge.com) 
+
+# Problema de Localização de Facilidades 
+
+O problema clássico das p-Medianas tem como objetivo definir a localização de p instalações (medianas) em uma rede de n nós, minimizando a soma das distâncias entre cada nó e a instalação mais próxima.
